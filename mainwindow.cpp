9fcc512bb65e6ff9df5f36992eebfafbd9449860
crazyrinchik <156314414+crazyrinchik@users.noreply.github.com>
2024-05-29 21:08:02 +0300

    Push button click to open table view

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,17 +1,23 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-#include "start_page.cpp"
-#include <QString>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-
+    setWindowTitle("Finance Master");
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::on_TableViewButton_clicked()
+{
+    TableView *tableView = new TableView(this);
+    tableView->show();
+}
+
+
