522f5e81b756ca7cfea1ec5165ba36c6f9cefa94
crazyrinchik <156314414+crazyrinchik@users.noreply.github.com>
2024-05-30 12:19:36 +0300

    Update mainwindow.cpp

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,9 +1,10 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
-MainWindow::MainWindow(QWidget *parent)
+MainWindow::MainWindow(const QString &filename, QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
+    , filename(filename)
 {
     ui->setupUi(this);
     setWindowTitle("Finance Master");
@@ -16,8 +17,9 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_TableViewButton_clicked()
 {
-    TableView *tableView = new TableView(this);
+    TableView *tableView = new TableView(filename, this);
     tableView->show();
 }
+
 
 
