203033b5a249f331e36f3de6178a4a38c28495b1
khaamr <144602803+khaamr@users.noreply.github.com>
2024-05-31 12:20:06 +0300

    Update mainwindow.h

--- mainwindow.h
+++ mainwindow.h
@@ -4,6 +4,7 @@
 #include <QMainWindow>
 #include "start_page.h"
 #include "tableview.h"
+#include "listview.h"
 #include <QString>
 #include <QWidget>
 #include <QVBoxLayout>
@@ -11,6 +12,8 @@
 #include <QMessageBox>
 #include <QSpinBox>
 #include <QStandardItemModel>
+#include <QtCharts/QChartView>
+#include <QtCharts/QPieSeries>
 
 namespace Ui {
 class MainWindow;
@@ -28,11 +31,22 @@ signals:
     void firstWindow();
 
 private slots:
+    void on_CashBackButton_clicked();
     void on_TableViewButton_clicked();
+    void on_budgetSpinBox_valueChanged(int value);
+
+    void on_LimitButton_clicked();
+
+    void on_ListViewButton_clicked();
+
+    void updatePieChart();
 
 private:
     Ui::MainWindow *ui;
     QString filename;
+    int budgetLimit;
+    QChartView *chartView;
+    QPieSeries *series;
 };
 
 #endif // MAINWINDOW_H
